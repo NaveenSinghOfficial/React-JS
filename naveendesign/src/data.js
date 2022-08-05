df7755a3c107a371d24e03a5191249eb6c33e07b
simmi kumari <simmikumari0104@gmail.com>
2022-08-05 20:56:41 +0530

    initial commit

---
+++ naveendesign/src/data.js
@@ -0,0 +1,79 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/photo_1517336714731_489689fd1ca8_9.jpeg",
+    title: "DIWALI SALE",
+    desc: "GET FLAT 10% OFF FOR NEW MAC.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://techcrunch.com/wp-content/uploads/2020/10/Apple_announce-iphone12pro_10132020.jpg",
+    title: "IPHONE OFFER",
+    desc: "THIS DIWALI GET A BRAND NEW IPHONE WITH 10% OFF.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://www.apple.com/newsroom/images/product/ipad/standard/apple_new-ipad-air_new-design_09152020_big.jpg.large.jpg",
+    title: "AN IPAD",
+    desc: "THIS DIWALI GET A NEW IPAD.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/photo_1517336714731_489689fd1ca8_9.jpeg",
+    title: "LAPTOP!",
+    cat:"electronics"
+  },
+  {
+    id: 2,
+    img: "https://images.moneycontrol.com/static-mcnews/2021/10/Apple-iPhone-13-4.jpg?impolicy=website&width=770&height=431",
+    title: "IPHONES",
+    cat:"mobiles"
+  },
+  {
+    id: 3,
+    img: "https://images.hindustantimes.com/tech/img/2022/06/05/960x540/Untitled_design_(60)_1654407815488_1654407834578.png",
+    title: "EARPHONE",
+    cat:"accessories"
+  },
+];
+
+export const popularProducts = [
+  {
+    id:1,
+    img:"https://5.imimg.com/data5/IZ/WE/TV/SELLER-76766217/dell-laptop-500x500.jpg",
+  },
+  {
+    id:2,
+    img:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mbp16-spacegray-select-202110?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1632788574000",
+  },
+  {
+    id:3,
+    img:"https://www.reliancedigital.in/medias/Lenovo-81WH007KIN-LAPTOP-492849666-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wxMDg2MnxpbWFnZS9qcGVnfGltYWdlcy9oNWIvaGI0Lzk4NDkyMzg2NTA5MTAuanBnfDY1ZTdjNmUxYzdiMDczOThhZDMzYjBlZGE1MmZlYTE5ZDZhOGNhZGEzYTU2NDk5YWQ5ODgxMTY4NjA3OTI4ZDE",
+  },
+  {
+    id:4,
+    img:"https://m.media-amazon.com/images/I/81di9mF9OVL._AC_SX355_.jpg",
+  },
+  {
+    id:5,
+    img:"https://m.media-amazon.com/images/I/712Ta39C3fL._SX522_.jpg",
+  },
+  {
+    id:6,
+    img:"https://stimg.cardekho.com/images/carexteriorimages/930x620/Ferrari/SF90-Stradale/7858/1591681679806/front-view-118.jpg",
+  },
+  {
+    id:7,
+    img:"https://static.wixstatic.com/media/644056_db6828aed4474408a35e8db49fd1c7bf~mv2.jpeg/v1/fill/w_1000,h_750,al_c,q_90,usm_0.66_1.00_0.01/644056_db6828aed4474408a35e8db49fd1c7bf~mv2.jpeg",
+  },
+  {
+    id:8,
+    img:"https://www.merriam-webster.com/reviews/wp-content/uploads/2021/11/Oculus-Quest-2-VR-Headset.jpg",
+  },
+]
